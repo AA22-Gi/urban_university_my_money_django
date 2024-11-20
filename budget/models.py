29c153139a4fa9a5ece4967de7358bf31077c0dc
AA22_Gi <gibert22@mail.ru>
2024-11-20 17:35:05 +0700

    В модель Transaction добавил поля transaction_type и константу TRANSACTION_TYPES

--- budget/models.py
+++ budget/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
+
 class Transaction(models.Model):
+    TRANSACTION_TYPES = (
+        ('income', 'Доход'),
+        ('expense', 'Расход'),
+    )
     title = models.CharField(max_length=30)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
+    transaction_type = models.CharField(max_length=10, choices=TRANSACTION_TYPES)
     date = models.DateField(auto_now_add=True)
