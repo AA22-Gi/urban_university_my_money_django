58fc7f33a8beac3f8aca2fc02b9d2c0a92394855
AA22_Gi <gibert22@mail.ru>
2024-11-20 22:44:35 +0700

    Добавлены переменные transactions, all_income, all_expenses, balance context

--- my_money/budget/views.py
+++ my_money/budget/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from .models import Transaction
 
-def  transaction_dashboard(request):
+
+def transaction_dashboard(request):
     # noinspection PyUnresolvedReferences
     transactions = Transaction.objects.all()
+    all_income = sum(transaction.amount for transaction in transactions if transaction.transaction_type == 'income')
+    all_expenses = sum(transaction.amount for transaction in transactions if transaction.transaction_type == 'expense')
+    balance = all_income - all_expenses
 
-    return render(request, 'budget/transaction_dashboard.html', {'transactions': transactions})
+    context = {'all_transactions': transactions,
+               'all_income': all_income,
+               'all_expenses': all_expenses,
+               'balance': balance}
+
+    return render(request, 'budget/transaction_dashboard.html', {'context': context})
