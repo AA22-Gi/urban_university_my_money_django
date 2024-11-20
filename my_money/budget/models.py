f0feb57aeac0f2793cd50295a1e7145d97207bfe
AA22_Gi <gibert22@mail.ru>
2024-11-20 22:45:17 +0700

    Добавлен магический метод __str__

--- my_money/budget/models.py
+++ my_money/budget/models.py
@@ -20,3 +20,6 @@ class Transaction(models.Model):
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     transaction_type = models.CharField(max_length=10, choices=TRANSACTION_TYPES)
     date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.title}'
