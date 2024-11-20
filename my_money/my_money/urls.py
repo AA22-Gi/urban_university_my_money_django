7b967608358b61970c62c874541fa00bd4836e1a
AA22_Gi <gibert22@mail.ru>
2024-11-20 19:08:34 +0700

    Импортировано представление transaction_dashboard Добавлен маршрут

--- my_money/my_money/urls.py
+++ my_money/my_money/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from budget.views import transaction_dashboard
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', transaction_dashboard),
 ]
