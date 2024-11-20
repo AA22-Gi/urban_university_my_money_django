0620c11eb9bd4925789a230d0a6da44a2f62787b
AA22_Gi <gibert22@mail.ru>
2024-11-20 17:32:23 +0700

    В модель Transaction добавил поля title, amount, date

--- budget/models.py
+++ budget/models.py
@@ -1,4 +1,6 @@
 from django.db import models
 
 class Transaction(models.Model):
-    pass
+    title = models.CharField(max_length=30)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField(auto_now_add=True)
