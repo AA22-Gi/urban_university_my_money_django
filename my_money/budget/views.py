1680bed76752bd1b886bd7b565a48f59d4682a42
AA22_Gi <gibert22@mail.ru>
2024-11-20 23:12:13 +0700

    К функции transaction_dashboard добавлена docstring

--- my_money/budget/views.py
+++ my_money/budget/views.py
@@ -3,6 +3,18 @@ from .models import Transaction
 
 
 def transaction_dashboard(request):
+    """
+    Отображает панель управления транзакциями и информацию о балансе пользователя.
+
+    Эта функция берет все транзакции из базы данных, вычисляет общую сумму доходов и расходов, а также баланс.
+    Затем она передает собранные данные в контексте для отображения на веб-странице через html-шаблон.
+
+    Параметры:
+        request (HttpRequest): Объект запроса, содержащий информацию о текущем запросе пользователя.
+
+    Возвращает:
+        HttpResponse: Ответ, содержащий шаблон с контекстом.
+    """
     # noinspection PyUnresolvedReferences
     transactions = Transaction.objects.all()
     all_income = sum(transaction.amount for transaction in transactions if transaction.transaction_type == 'income')
