3ef89f1b59393ea61712bd0f666c1c757a1c770e
AA22_Gi <gibert22@mail.ru>
2024-11-21 18:34:20 +0700

    Добавлена словарь labels для отображения заголовков полей формы на русском языке

--- my_money/budget/forms.py
+++ my_money/budget/forms.py
@@ -5,3 +5,8 @@ class TransactionForm(forms.ModelForm):
     class Meta:
         model = Transaction
         fields = '__all__'
+        labels = {
+            'title': 'Название',
+            'amount': 'Сумма',
+            'transaction_type': 'Тип транзакции',
+        }
