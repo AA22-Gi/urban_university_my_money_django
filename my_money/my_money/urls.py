eb6ecbe782c6a62455c9ca143108b176c1ce02d7
AA22_Gi <gibert22@mail.ru>
2024-11-21 17:57:40 +0700

    Добавлен динамический URL

--- my_money/my_money/urls.py
+++ my_money/my_money/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from budget.views import transaction_dashboard
+from budget.views import transaction_dashboard, edit_transaction
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', transaction_dashboard, name='transaction_dashboard'),
-    path('edit_transaction', transaction_dashboard, name='edit_transaction'),
+    path('edit_transaction/<int:id_transaction>/', edit_transaction, name='edit_transaction'),
 ]
