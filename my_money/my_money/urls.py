bb51f501328ae6b04b18b5c4a80f8c5e35ec2928
AA22_Gi <gibert22@mail.ru>
2024-11-23 08:44:31 +0700

    Импортирована функция delete_transaction Добавлен URL

--- my_money/my_money/urls.py
+++ my_money/my_money/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from budget.views import transaction_dashboard, edit_transaction, add_transaction
+from budget.views import transaction_dashboard, edit_transaction, add_transaction, delete_transaction
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', transaction_dashboard, name='transaction_dashboard'),
     path('add_transaction', add_transaction, name='add_transaction'),
     path('edit_transaction/<int:id_transaction>/', edit_transaction, name='edit_transaction'),
+    path('delete_transaction/<int:id_transaction>/', delete_transaction, name='delete_transaction'),
 ]
