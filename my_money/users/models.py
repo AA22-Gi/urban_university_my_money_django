d5c97741d09ddafadeb2b8166967c7edc485fbdb
AA22_Gi <gibert22@mail.ru>
2024-11-25 18:50:17 +0700

    Добавлена константа GENDER_CHOICES У полей name, surname уменьшена длинна строки. У gender добавлен параметр choices

--- my_money/users/models.py
+++ my_money/users/models.py
@@ -1,14 +1,18 @@
 from django.db import models
 
+
 class User(models.Model):
-    name = models.CharField(max_length=100)  # Имя
-    surname = models.CharField(max_length=100)  # Фамилия
+    GENDER_CHOICES = [
+        ('M', 'мужской'),
+        ('F', 'женский'),
+    ]
+
+    name = models.CharField(max_length=20)  # Имя
+    surname = models.CharField(max_length=20)  # Фамилия
     age = models.PositiveIntegerField(null=True)  # Возраст
-    gender = models.CharField(max_length=10)  # Пол
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)  # Пол
     email = models.EmailField(unique=True)  # Почта
-    password = models.CharField(max_length=128) # Пароль
+    password = models.CharField(max_length=128)  # Пароль
 
     def __str__(self):
         return f"{self.name} {self.surname}"
-
-
