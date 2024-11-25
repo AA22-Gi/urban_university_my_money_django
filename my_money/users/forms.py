89988d6c3579ecf1ad7926f6134cdd87e5a290d8
AA22_Gi <gibert22@mail.ru>
2024-11-25 21:24:45 +0700

    docstring

--- my_money/users/forms.py
+++ my_money/users/forms.py
@@ -1,5 +1,13 @@
 from django import forms
 
 class LoginForm(forms.Form):
+    """
+    Форма для входа пользователя.
+
+    Поля:
+    - email: Адрес электронной почты пользователя.
+    - password: Пароль пользователя (отображается как текст).
+    """
     email = forms.EmailField(label='Email', max_length=50)
     password = forms.CharField(label='Пароль', widget=forms.TextInput)
+
