8fc37b645249aaa0b18ac88cbbf0a028f80fe705
AA22_Gi <gibert22@mail.ru>
2024-11-25 21:22:35 +0700

    Импортирована форма LoginForm Написана функция-представление login

--- my_money/users/views.py
+++ my_money/users/views.py
@@ -1,4 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import LoginForm
+
 
 def home(request):
     return render(request, 'users_html/home.html')
+
+
+def login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            # Обработка данных, аутентификация пользователя и т.п.
+            return redirect('transaction_dashboard')  # Перенаправление на домашнюю страницу
+    else:
+        form = LoginForm()
+
+    return render(request, 'users_html/login.html', {'form': form})
