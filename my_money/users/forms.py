41ca90e3d0be92c36b95649dadd000259409014b
AA22_Gi <gibert22@mail.ru>
2024-11-25 20:31:04 +0700

    Написана основная логика формы

--- my_money/users/forms.py
+++ my_money/users/forms.py
@@ -1,1 +1,5 @@
 from django import forms
+
+class LoginForm(forms.Form):
+    email = forms.EmailField(label='Email', max_length=50)
+    password = forms.CharField(label='Пароль', widget=forms.TextInput)
