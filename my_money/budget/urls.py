7093ee0193cf40084b443f077f406d15636aec81
AA22_Gi <gibert22@mail.ru>
2024-11-25 19:28:01 +0700

    Перенес адреса из основной директории в приложение budget

---
+++ my_money/budget/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import transaction_dashboard, edit_transaction, add_transaction, delete_transaction
+
+urlpatterns = [
+    path('', transaction_dashboard, name='transaction_dashboard'),
+    path('add_transaction', add_transaction, name='add_transaction'),
+    path('edit_transaction/<int:id_transaction>/', edit_transaction, name='edit_transaction'),
+    path('delete_transaction/<int:id_transaction>/', delete_transaction, name='delete_transaction'),
+]
