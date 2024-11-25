0518b66487f966927a2432484dd2a95b194e0ec1
AA22_Gi <gibert22@mail.ru>
2024-11-25 18:47:12 +0700

    Добавлено поле surname и строковое представление модели

--- my_money/users/models.py
+++ my_money/users/models.py
@@ -2,11 +2,13 @@ from django.db import models
 
 class User(models.Model):
     name = models.CharField(max_length=100)  # Имя
+    surname = models.CharField(max_length=100)  # Фамилия
     age = models.PositiveIntegerField(null=True)  # Возраст
     gender = models.CharField(max_length=10)  # Пол
     email = models.EmailField(unique=True)  # Почта
     password = models.CharField(max_length=128) # Пароль
 
-
+    def __str__(self):
+        return f"{self.name} {self.surname}"
 
 
