729ab73ddc030aa99c0a48075c77ccfdd0d372a9
AA22_Gi <gibert22@mail.ru>
2024-11-26 14:22:09 +0700

    Написана функция-представление register

--- my_money/users/views.py
+++ my_money/users/views.py
@@ -1,18 +1,29 @@
 from django.shortcuts import render, redirect
-from .forms import LoginForm
+from .forms import UserRegistrationForm
 
 
 def home(request):
-    return render(request, 'users_html/home.html')
+    return render(request, 'users/home.html')
 
 
 def login(request):
     if request.method == 'POST':
-        form = LoginForm(request.POST)
+        form = UserRegistrationForm(request.POST)
         if form.is_valid():
             # Обработка данных, аутентификация пользователя и т.п.
             return redirect('transaction_dashboard')  # Перенаправление на домашнюю страницу
     else:
-        form = LoginForm()
+        form = UserRegistrationForm()
 
-    return render(request, 'users_html/login.html', {'form': form})
+    return render(request, 'users/login.html', {'form': form})
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')  # Перенаправление на страницу авторизации после регистрации
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'users/register.html', {'form': form})
