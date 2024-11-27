c79f57e94ca5b1a481f5d78dc7fe90dad3a2cf3f
AA22_Gi <gibert22@mail.ru>
2024-11-27 07:14:24 +0700

    Убрал параметр default у user

--- my_money/budget/models.py
+++ my_money/budget/models.py
@@ -22,7 +22,7 @@ class Transaction(models.Model):
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     transaction_type = models.CharField(max_length=10, choices=TRANSACTION_TYPES)
     date = models.DateField(auto_now_add=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.title}'
