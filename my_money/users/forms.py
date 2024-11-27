a6568d45309824d816cea7be9bd2d563dd9477cb
AA22_Gi <gibert22@mail.ru>
2024-11-27 10:05:08 +0700

    Переписал форму

--- my_money/users/forms.py
+++ my_money/users/forms.py
@@ -1,14 +1,15 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
 from .models import User
 
-
-class UserRegistrationForm(forms.ModelForm):
+class RegisterForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['name', 'age', 'email']
-
-
-class UserLoginForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ['name', 'password']
+        fields = ['username', 'age', 'gender', 'email', 'password1', 'password2']
+        labels = {
+            'age': 'Возраст',
+            'gender': 'Пол',
+            'email': 'Электронная почта',
+            'password1': 'Пароль',
+            'password2': 'Повторите пароль',
+        }
