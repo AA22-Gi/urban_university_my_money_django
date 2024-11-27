bc1be72dae2edd2c420c863ac7098d543830025b
AA22_Gi <gibert22@mail.ru>
2024-11-27 10:04:40 +0700

    Переписал модель

--- my_money/users/models.py
+++ my_money/users/models.py
@@ -1,30 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser ):
+    GENDER_CHOICES = [('M', 'мужской'), ('F', 'женский')]
+
+    # Поля, которые вы хотите оставить
+    age = models.PositiveIntegerField(null=True, blank=True)
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES, blank=True)
 
 
-class User(models.Model):
-    """
-        Модель для представления пользователя.
-
-        Атрибуты:
-            name (str): Имя пользователя.
-            surname (str): Фамилия пользователя.
-            age (int, optional): Возраст пользователя, может быть пустым.
-            gender (str): Пол пользователя ('M' для мужского, 'F' для женского).
-            email (str): Электронная почта пользователя, должна быть уникальной.
-            password (str): Пароль пользователя.
-        """
-
-    GENDER_CHOICES = [
-        ('M', 'мужской'),
-        ('F', 'женский'),
-    ]
-
-    name = models.CharField(max_length=20)  # Имя
-    surname = models.CharField(max_length=20)  # Фамилия
-    age = models.PositiveIntegerField(null=True)  # Возраст
-    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)  # Пол
-    email = models.EmailField(unique=True)  # Почта
-    password = models.CharField(max_length=128)  # Пароль
-
-    def __str__(self):
-        return f"{self.name} {self.surname}"
+    # Переопределяем поля first_name и last_name, чтобы они не использовались
+    first_name = None
+    last_name = None
