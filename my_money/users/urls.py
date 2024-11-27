4e208bacc36a64250042264a1925f8ee5050347e
AA22_Gi <gibert22@mail.ru>
2024-11-27 07:10:53 +0700

    Добавлен URL

--- my_money/users/urls.py
+++ my_money/users/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import home, login, register
+from .views import home, login_view, register, user_exit
 
 urlpatterns = [
     path('', home, name='home'),
-    path('login/', login, name='login'),
     path('register/', register, name='register'),
-
+    path('login/', login_view, name='login'),
+    path('exit/', user_exit, name='exit'),
 ]
