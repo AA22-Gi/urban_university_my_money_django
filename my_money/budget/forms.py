fc3dcea3befe686d1cc74f9b029d06585711e123
AA22_Gi <gibert22@mail.ru>
2024-11-28 19:25:38 +0700

    Добавлена документация

--- my_money/budget/forms.py
+++ my_money/budget/forms.py
@@ -1,12 +1,23 @@
 from django import forms
 from .models import Transaction
 
+
 class TransactionForm(forms.ModelForm):
+    """
+    Форма для создания и редактирования транзакции.
+
+    Эта форма основана на модели `Transaction` и позволяет пользователю
+    вводить данные для названия, суммы и типа транзакции.
+
+    Атрибуты:
+        Meta (class): Внутренний класс, определяющий модель, поля и метки.
+    """
+
     class Meta:
-        model = Transaction
-        fields = ['title', 'amount', 'transaction_type']
+        model = Transaction  # Модель, к которой относится форма
+        fields = ['title', 'amount', 'transaction_type']  # Поля для ввода данных
         labels = {
-            'title': 'Название',
-            'amount': 'Сумма',
-            'transaction_type': 'Тип транзакции',
+            'title': 'Название',  # Метка для поля названия
+            'amount': 'Сумма',  # Метка для поля суммы
+            'transaction_type': 'Тип транзакции',  # Метка для поля типа транзакции
         }
