c13d2ad72b1771d84f07d2536be8ce798a46c4c5
AA22_Gi <gibert22@mail.ru>
2024-11-28 19:14:57 +0700

    Расширена документация

--- my_money/budget/models.py
+++ my_money/budget/models.py
@@ -4,25 +4,33 @@ from users.models import User
 
 class Transaction(models.Model):
     """
-    Модель для представления финансовых транзакции.
+    Модель транзакции.
+
+    Эта модель представляет транзакцию, которая может быть либо
+    доходом, либо расходом. Она содержит информацию о названии,
+    сумме, типе транзакции, дате и привязанному пользователю.
 
     Атрибуты:
-        title (str): Описание транзакции.
-        amount (float): Сумма транзакции (максимум 10 цифр, 2 из которых после запятой).
-        transaction_type (str): Тип транзакции (может быть 'income' (Доход) или 'expense' (Расход)).
-        date (DateField): Дата создания транзакции (устанавливается автоматически при добавлении).
-        user (User): Пользователь, которому принадлежит транзакция.
+        title (str): Название транзакции.
+        amount (Decimal): Сумма транзакции.
+        transaction_type (str): Тип транзакции (доход или расход).
+        date (DateField): Дата создания транзакции (автоматически устанавливается).
+        user (ForeignKey): Связанный пользователь (пользователь, создавший транзакцию).
     """
 
     TRANSACTION_TYPES = (
         ('income', 'Доход'),
         ('expense', 'Расход'),
     )
-    title = models.CharField(max_length=30)
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
-    transaction_type = models.CharField(max_length=10, choices=TRANSACTION_TYPES)
-    date = models.DateField(auto_now_add=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    title = models.CharField(max_length=30)  # Название транзакции
+    amount = models.DecimalField(max_digits=10, decimal_places=2)  # Сумма транзакции
+    transaction_type = models.CharField(max_length=10, choices=TRANSACTION_TYPES)  # Тип транзакции
+    date = models.DateField(auto_now_add=True)  # Дата создания транзакции
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # Пользователь, создавший транзакцию
 
     def __str__(self):
+        """
+        Возвращает строковое представление транзакции.
+        """
         return f'{self.title}'
