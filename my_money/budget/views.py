b029e49b7b46844f7727e8d1ea73ef8d8a8bab46
AA22_Gi <gibert22@mail.ru>
2024-11-28 19:14:05 +0700

    Код приведен к стандарту PEP8 В функции dd_transaction заменена строка для сохранения транзакции transaction.save()

--- my_money/budget/views.py
+++ my_money/budget/views.py
@@ -7,26 +7,28 @@ def transaction_dashboard(request):
     """
     Отображает панель управления транзакциями и информацию о балансе пользователя.
 
-    Эта функция берет все транзакции из базы данных, вычисляет общую сумму доходов и расходов, а также баланс.
-    Затем она передает собранные данные в контексте для отображения на веб-странице через html-шаблон.
+    Эта функция берет все транзакции из базы данных, вычисляет общую сумму доходов и расходов,
+    а также баланс. Затем она передает собранные данные в контексте для отображения на веб-странице
+    через HTML-шаблон.
 
-    Параметры:
+    Args:
         request (HttpRequest): Объект запроса, содержащий информацию о текущем запросе пользователя.
 
-    Возвращает:
+    Returns:
         HttpResponse: Ответ, содержащий шаблон с контекстом.
     """
     # Получаем транзакции только для текущего пользователя
-    # noinspection PyUnresolvedReferences
     transactions = Transaction.objects.filter(user=request.user)
     all_income = sum(transaction.amount for transaction in transactions if transaction.transaction_type == 'income')
     all_expenses = sum(transaction.amount for transaction in transactions if transaction.transaction_type == 'expense')
     balance = all_income - all_expenses
 
-    context = {'all_transactions': transactions,
-               'all_income': all_income,
-               'all_expenses': all_expenses,
-               'balance': balance}
+    context = {
+        'all_transactions': transactions,
+        'all_income': all_income,
+        'all_expenses': all_expenses,
+        'balance': balance
+    }
 
     return render(request, 'budget/transaction_dashboard.html', {'context': context})
 
@@ -39,22 +41,23 @@ def add_transaction(request):
     если форма действительна. В случае успешного сохранения происходит перенаправление
     на панель управления транзакциями.
 
-    Параметры:
+    Args:
         request (HttpRequest): Объект запроса, содержащий информацию о текущем запросе.
 
-    Возвращает:
-        HttpResponse: Ответ с отрендеренным шаблоном 'budget_html/add_transaction.html',
-                          содержащим форму для добавления новой транзакции.
+    Returns:
+        HttpResponse: Ответ с отрендеренным шаблоном 'budget/add_transaction.html',
+                      содержащим форму для добавления новой транзакции.
     """
     if request.method == 'POST':
         form = TransactionForm(request.POST)
         if form.is_valid():
             transaction = form.save(commit=False)
             transaction.user = request.user  # Устанавливаем текущего пользователя
-            form.save()
+            transaction.save()
             return redirect('transaction_dashboard')
     else:
         form = TransactionForm()
+
     return render(request, 'budget/add_transaction.html', {'form': form})
 
 
@@ -63,15 +66,16 @@ def edit_transaction(request, id_transaction):
     Обрабатывает редактирование существующей транзакции.
 
     Эта функция получает транзакцию по ее идентификатору,
-    отображает форму редактирования и сохраняет изменения, если форма валидна.
-    В случае успешного сохранения происходит перенаправление на панель управления транзакциями.
+    отображает форму редактирования и сохраняет изменения,
+    если форма валидна. В случае успешного сохранения происходит
+    перенаправление на панель управления транзакциями.
 
-    Параметры:
+    Args:
         request (HttpRequest): Объект запроса, содержащий информацию о текущем запросе.
         id_transaction (int): Идентификатор транзакции, которую необходимо редактировать.
 
-    Возвращает:
-        HttpResponse: Ответ с шаблоном 'budget_html/edit_transaction.html',
+    Returns:
+        HttpResponse: Ответ с шаблоном 'budget/edit_transaction.html',
                       содержащим форму для редактирования транзакции.
     """
     transaction = get_object_or_404(Transaction, id=id_transaction)
@@ -82,30 +86,28 @@ def edit_transaction(request, id_transaction):
             return redirect('transaction_dashboard')
     else:
         form = TransactionForm(instance=transaction)
+
     return render(request, 'budget/edit_transaction.html', {'form': form})
 
 
 def delete_transaction(request, id_transaction):
     """
-       Обрабатывает удаление транзакции.
+    Обрабатывает удаление транзакции.
 
-       Если метод запроса - POST, транзакция с указанным идентификатором
-       будет удалена из базы данных, и пользователь будет перенаправлен
-       на страницу с информацией о транзакциях. Если метод запроса
-       не является POST, будет отображена страница подтверждения удаления
-       с информацией о транзакции.
+    Если метод запроса - POST, транзакция с указанным идентификатором
+    будет удалена из базы данных, и пользователь будет перенаправлен
+    на страницу с информацией о транзакциях. Если метод запроса
+    не является POST, будет отображена страница подтверждения удаления
+    с информацией о транзакции.
 
-       Параметры:
-       request : HttpRequest
-           Объект запроса, содержащий информацию о текущем запросе.
+    Args:
+        request (HttpRequest): Объект запроса, содержащий информацию о текущем запросе.
+        id_transaction (int): Идентификатор транзакции, которую необходимо удалить.
 
-       id_transaction : int
-           Идентификатор транзакции, которую необходимо удалить.
-
-       Возвращает:
-           HttpResponse: Перенаправление на страницу с транзакциями после успешного
-                         удаления или рендеринг страницы подтверждения удаления.
-       """
+    Returns:
+        HttpResponse: Перенаправление на страницу с транзакциями после успешного
+                      удаления или рендеринг страницы подтверждения удаления.
+    """
     transaction = get_object_or_404(Transaction, id=id_transaction)
     if request.method == 'POST':
         transaction.delete()
