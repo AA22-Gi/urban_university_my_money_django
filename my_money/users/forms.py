073c51f1380d0aea3065dc74e378852680e9af1a
AA22_Gi <gibert22@mail.ru>
2024-11-28 19:25:29 +0700

    Добавлена документация

--- my_money/users/forms.py
+++ my_money/users/forms.py
@@ -3,6 +3,22 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import User
 
 class RegisterForm(UserCreationForm):
+    """
+    Форма регистрации нового пользователя.
+
+    Эта форма расширяет стандартную форму UserCreationForm и позволяет
+    пользователю вводить дополнительную информацию, такую как возраст и пол,
+    помимо стандартных полей.
+
+    Атрибуты:
+        Meta: Класс, который определяет модель, которую будет использовать форма,
+              и список полей для ввода.
+              Поля:
+                  - model (User): Модель пользователя, к которой относится форма.
+                  - fields (list): Список полей, которые будут включены в форму.
+                  - labels (dict): Словарь меток для полей формы.
+    """
+
     class Meta:
         model = User
         fields = ['username', 'age', 'gender', 'email', 'password1', 'password2']
