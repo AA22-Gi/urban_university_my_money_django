5cf0454f6e79369b9af3c195bac7240d4e62d7ef
AA22_Gi <gibert22@mail.ru>
2024-11-28 19:14:29 +0700

    Добавлена документация

--- my_money/users/models.py
+++ my_money/users/models.py
@@ -1,13 +1,29 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-class User(AbstractUser ):
-    GENDER_CHOICES = [('M', 'мужской'), ('F', 'женский')]
 
-    # Поля, которые вы хотите оставить
+class User(AbstractUser ):
+    """
+    Пользовательская модель для учета дополнительной информации о пользователе.
+
+    Эта модель расширяет стандартную модель пользователя Django, добавляя
+    поля для возраста и пола. Также переопределяются поля 'first_name'
+    и 'last_name', чтобы они не использовались в данной модели.
+
+    Атрибуты:
+        age (PositiveIntegerField): Возраст пользователя. Может быть null или пустым.
+        gender (CharField): Пол пользователя. Может принимать значения 'M' (мужской) или 'F' (женский).
+
+    Примечание:
+        Переопределенные поля 'first_name' и 'last_name' убраны из модели, чтобы избежать их использования.
+    """
+    GENDER_CHOICES = [
+        ('M', 'мужской'),
+        ('F', 'женский')
+    ]
+
     age = models.PositiveIntegerField(null=True, blank=True)
     gender = models.CharField(max_length=10, choices=GENDER_CHOICES, blank=True)
-
 
     # Переопределяем поля first_name и last_name, чтобы они не использовались
     first_name = None
